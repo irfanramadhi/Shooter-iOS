9ecb73b83fc2e700e39b54855bd261b187bada8d
Irfan Ramadhi <ramadhiirfan@yahoo.co.id>
2018-05-16 22:11:40 +0700

    Unity iOS Shooter Game

---
+++ TapToShoot/Assets/Scripts/TimedSpawn.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimedSpawn : MonoBehaviour
+{
+
+    public GameObject EnemyPrefab;
+
+    public Vector3 center;
+    public Vector3 size;
+
+	// Use this for initialization
+	void Start ()
+    {
+
+        SpawnEnemy();
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+        if(Input.GetKey(KeyCode.Q))
+        {
+            SpawnEnemy();
+        }
+
+	}
+
+    public void SpawnEnemy()
+    {
+
+        Vector3 pos = center + new Vector3(Random.Range(-size.x / 2 , size.x /2), Random.Range(-size.y / 2, size.y / 2), Random.Range(-size.z / 2, size.z / 2));
+        Instantiate(EnemyPrefab, pos, Quaternion.identity);
+
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = new Color(1, 0, 0, 0.5f);
+        Gizmos.DrawCube(center, size);
+    }
+
+}
---
+++ TapToShoot/Assets/Scripts/TouchManager.cs
@@ -0,0 +1,38 @@
+﻿ using UnityEngine;
+using System.Collections;
+
+public class TouchManager : MonoBehaviour {
+
+	public GameObject explosion;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetMouseButton(0))
+		{
+			Vector3 mousePosFar = new Vector3(Input.mousePosition.x,
+												 Input.mousePosition.y,
+												 Camera.main.farClipPlane);
+			Vector3 mousePosNear = new Vector3(Input.mousePosition.x,
+												 Input.mousePosition.y,
+												 Camera.main.nearClipPlane);
+			Vector3 mousePosF = Camera.main.ScreenToWorldPoint(mousePosFar);
+			Vector3 mousePosN = Camera.main.ScreenToWorldPoint(mousePosNear);
+			//Debug.DrawRay(mousePosN,mousePosF-mousePosN, Color.green);
+
+
+			RaycastHit hit;
+
+        	if (Physics.Raycast(mousePosN, mousePosF-mousePosN, out hit))
+        	{
+            	//Instantiate(explosion, hit.transform.position, Quaternion.identity);
+            	Destroy(hit.transform.gameObject);
+            }
+		}
+	}
+}
